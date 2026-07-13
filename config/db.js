e1bd35b06c96a2f530d7bbc3ec8bbd3503474c7c
Jujjavarapu Sai Namratha <sainamrathajujjavarapu9@gmail.com>
2026-07-13 16:13:19 +0530

    Add DNS server configuration for MongoDB connection

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-
+const dns=require("dns")
+dns.setServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      process.env.MONGO_URI || mongodb+srv://mongodb+srv://sainamrathajujjavarapu9:namratha@123@cluster0.pcfj1ad.mongodb.net/
+      process.env.MONGO_URI || mongodb+srv://mongodb+srv://sainamrathajujjavarapu9:namratha@123@cluster0.pcfj1ad.mongodb.net/namratha
     );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
