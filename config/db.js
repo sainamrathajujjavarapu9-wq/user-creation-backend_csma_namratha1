f24b81129c1638948806d44e49a3ff3823347e02
navyanunna7-prog <navyanunna7@gmail.com>
2026-07-13 15:20:03 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      process.env.MONGO_URI || "mongodb://localhost:27017/userProfileDB"
+      process.env.MONGO_URI || mongodb+srv://navyanunna:Navya25@B9@cluster0.dvxw9kb.mongodb.net/Navya
     );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
